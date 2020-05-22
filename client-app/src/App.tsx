17c76eb91240c04b0b88a173aa6fe8928093ce06
Javier Encina <encinajavier7@gmail.com>
2020-05-21 23:25:24 -0300

    Add axios and Sementic UI

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { Header, Icon, List } from 'semantic-ui-react';
+import axios from 'axios'
+class App extends Component  {
+  state = {
+    values: []
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount()
+  {
+    axios.get('https://localhost:44395/api/values').then((values)=>{
+      this.setState({
+        values: values.data
+      })
+    });
+
+  }
+
+  render(){
+    return (
+      <div>
+      <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>Reactivities</Header.Content>
+      </Header>
+      <List>
+       { this.state.values.map((value: any) =>(
+              <List.Item key={ value.id }>{value.name}</List.Item>
+            ))}
+      </List>
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
