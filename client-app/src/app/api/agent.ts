63a59ebdd463580e0d7950dd2ec482f4585074f5
Javier Encina <encinajavier7@gmail.com>
2020-05-26 23:39:26 -0300

    Crud implementation completed

---
+++ client-app/src/app/api/agent.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosResponse } from "axios"
+import { IActivity } from "../models/activity";
+import { resolve } from "dns";
+
+axios.defaults.baseURL = 'https://localhost:44395/api';
+
+const respondBody = (response:AxiosResponse) => response.data;
+const sleep = (ms: number) => (response:AxiosResponse) => new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
+
+const requests = {
+    get: (url: string) => axios.get(url).then(sleep(1000)).then(respondBody),
+    post: (url:string, body: {}) => axios.post(url, body).then(sleep(1000)).then(respondBody),
+    put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(respondBody),
+    delete:(url: string) => axios.delete(url).then(sleep(1000)).then(respondBody)
+};
+
+const Activities = {
+    list: ():Promise<IActivity[]> => requests.get('/activities'),
+    details: (id:string) => requests.get(`/activities/${id}`),
+    create: (activity:IActivity) => requests.post('activities', activity),
+    update: (activity:IActivity) => requests.put(`activities/${activity.id}`, activity),
+    delete: (id: string) => requests.delete(`activities/${id}`)
+}
+
+
+export default {
+    Activities
+}
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
 import { Item, Image, Button, Label, Segment } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 
-interface IProps{
+interface IProps {
     activities: IActivity[],
     selectActivity: (id: string) => void;
-    deleteActivity: (id: string) => void;
+    deleteActivity: (e: SyntheticEvent<HTMLButtonElement>, id: string) => void,
+    submitting: boolean,
+    target: string
 }
 
-const ActivityList: React.FC<IProps> = ({activities, selectActivity, deleteActivity}) =>{
+const ActivityList: React.FC<IProps> = ({ activities, selectActivity, deleteActivity,
+    submitting, target }) => {
     return (
         <Segment clearing>
-             <Item.Group divided>
-                 {activities.map(activity => (
+            <Item.Group divided>
+                {activities.map(activity => (
                     <Item key={activity.id}>
                         <Item.Content>
                             <Item.Header as='a'>{activity.title}</Item.Header>
@@ -22,13 +25,19 @@ const ActivityList: React.FC<IProps> = ({activities, selectActivity, deleteActiv
                                 <div>{activity.city}, {activity.venue}</div>
                             </Item.Description>
                             <Item.Extra>
-                                <Button onClick={ () => selectActivity(activity.id)} floated='right' content='View' color='blue' />
-                                <Button onClick={ () => deleteActivity(activity.id)} floated='right' content='Delete' color='red' />
-                                <Label basic content={activity.category}/>
+                                <Button onClick={() => selectActivity(activity.id)} floated='right' content='View' color='blue' />
+                                <Button
+                                    onClick={(e) => deleteActivity(e, activity.id)}
+                                    loading={target === activity.id && submitting}
+                                    name={activity.id}
+                                    floated='right'
+                                    content='Delete'
+                                    color='red' />
+                                <Label basic content={activity.category} />
                             </Item.Extra>
                         </Item.Content>
                     </Item>
-                 ))}
+                ))}
             </Item.Group>
         </Segment>
     );
